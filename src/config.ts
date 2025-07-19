db68f454216dc4fbf0c7a0bf2067451db37816fe
Ahmad Nur <ahmadnursahid12345@gmail.com>
2025-07-19 11:18:39 +0700

    refactor:  use TypeScript

---
+++ src/config.ts
@@ -0,0 +1,63 @@
+export const SITE: Site = {
+	title: "Ahmaadn",
+	author: "Ahmad nur sahid",
+	email: "ahmadnur415@gmail.com",
+	position: "Fullstack",
+	shortDescription:
+		"Seorang software engineer dengan pengalaman dalam mengembangkan aplikasi web menggunakan berbagai teknologi seperti Vue.js, FastApi, dan PostgreSQL.",
+	description: [
+		"Hai, saya Ahmad Nur Sahid. Saya seorang software engineer yang berbasis di Indonesia. Saya memiliki pengalaman dalam membangun aplikasi web menggunakan teknologi modern seperti Vue.js, FastApi, dan PostgreSQL.",
+		"Saat ini saya sedang menempuh pendidikan di Institut Teknologi Garut. Saya selalu mencari kesempatan baru untuk belajar dan berkembang sebagai mahasiswa. Saya juga terbuka untuk pekerjaan freelance, jadi jangan ragu untuk menghubungi saya jika Anda memiliki proyek yang ingin Anda kerjakan.",
+	],
+};
+
+export const LINKS: Link[] = [
+	{ label: "Github", url: "https://github.com/ahmaadn", icon: "simple-icons:github" },
+	{
+		label: "Resume",
+		url: "./resume.pdf",
+		icon: "simple-icons:googledocs",
+		download: "./resume.pdf",
+	},
+];
+
+export const EDUCATION: Education[] = [
+	{
+		from: "2022",
+		to: "Now",
+		institute: "Institut Teknologi Garut",
+		course: "Teknik Informatika",
+		location: "Garut, Jawa Barat",
+	},
+	{
+		from: "2019",
+		to: "2022",
+		institute: "SMKS AL-Farisi Leles",
+		course: "Teknik Komputer Dan Jaringan",
+		location: "Leles, Garut",
+	},
+];
+
+export const SKILLS: Skill[] = [
+	{ name: "Python", icon: "logos:python" },
+	{ name: "pyTorch", icon: "logos:pytorch-icon" },
+	{ name: "Jupyter", icon: "logos:jupyter" },
+	{ name: "Vue", icon: "logos:vue" },
+	{ name: "HTML5", icon: "logos:html-5" },
+	{ name: "CSS", icon: "logos:css-3" },
+	{ name: "JavaScript", icon: "logos:javascript" },
+	{ name: "Astro", icon: "logos:astro-icon" },
+	{ name: "TensorFlow", icon: "logos:tensorflow" },
+	{ name: "PHP", icon: "logos:php" },
+	{ name: "Laravel", icon: "logos:laravel" },
+	{ name: "FastApi", icon: "logos:fastapi-icon" },
+	{ name: "PostgreSQL", icon: "logos:postgresql" },
+	{ name: "MySQL", icon: "logos:mysql" },
+	{ name: "TypeScript", icon: "logos:typescript-icon" },
+	{ name: "Bootstrap", icon: "logos:bootstrap" },
+	{ name: "Dart", icon: "logos:dart" },
+	{ name: "Flutter", icon: "logos:flutter" },
+	{ name: "ScikitLearn", icon: "devicon:scikitlearn" },
+	{ name: "TailwindCSS", icon: "logos:tailwindcss-icon" },
+	{ name: "Pandas", icon: "logos:pandas-icon" },
+];
