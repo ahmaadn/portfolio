d7c5eb3cd5410c6770e77a2d0a40bef75b6ec95c
Ahmad NuR <ahmadnursahid12345@gmail.com>
2026-05-15 20:37:14 +0700

    feat: 404 page localization

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -15,10 +15,18 @@ export const ui = {
 			"Saya memiliki pengalaman dalam merancang dan mengimplementasikan API yang kuat, serta mengelola database untuk memastikan kinerja yang optimal. Selain itu, saya juga memiliki keahlian dalam menganalisis data menggunakan berbagai alat dan teknik untuk membantu pengambilan keputusan yang lebih baik.",
 			"Saya juga terbuka untuk pekerjaan, jadi jangan ragu untuk menghubungi saya jika Anda memiliki proyek yang ingin Anda kerjakan.",
 		],
+
 		"position.backend_develover": "Backend Developer",
 		"position.data_scientist": "Data Scientist",
-		"common.button.viewAll": "Lihat semua proyek",
+
+		"common.button.view_all": "Lihat semua proyek",
+		"common.button.back_to_home": "Kembali ke Home",
+
 		"common.footer.tagline": "Open for freelance and full-time opportunities",
+
+		"404.title": "Halaman tidak ditemukan.",
+		"404.description": "Tautan yang kamu buka mungkin sudah dipindah, dihapus, atau URL-nya tidak valid. Kamu bisa kembali ke beranda atau jelajahi daftar project terbaru.",
+
 	},
 	en: {
 		"site.title": "Ahmaadn",
@@ -34,8 +42,13 @@ export const ui = {
 		"position.backend_develover": "Backend Developer",
 		"position.data_scientist": "Data Scientist",
 
-		"common.button.viewAll": "View all projects",
+		"common.button.view_all": "View all projects",
+		"common.button.back_to_home": "back to home",
+
 		"common.footer.tagline": "Open for freelance and full-time opportunities",
+
+		"404.title": "Page not found.",
+		"404.description": "The link you're trying to access may have been moved, deleted, or the URL may be invalid. You can return to the homepage or browse the list of recent projects..",
 	},
 } as const;
 
